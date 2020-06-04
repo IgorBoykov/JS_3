b4e26c51018d5babdca94deb9e29be09839bee19
IgorBoykov <62951857+IgorBoykov@users.noreply.github.com>
2020-06-04 10:05:44 +0300

    Update Task1.js

--- JS_3/Task1.js
+++ JS_3/Task1.js
@@ -1,3 +1,5 @@
-var products = [];
+var products = [ 10, 2, 13, 9, 7 ];
 var l = products.length;
-console.log(l);
+console.log('Довжина масиву ' + l);
+var l2 = products[products.length - 1];
+console.log('Останній елемент масиву ' + l2);
